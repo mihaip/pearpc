b2118247c57ba559471d885fb22b9d62d37f6b1f
Mihai Parparita <mihai@persistent.info>
2025-01-12 20:20:47 -0800

    Default to disabling logging in JS threading stubs

--- src/system/osapi/js/systhread.cc
+++ src/system/osapi/js/systhread.cc
@@ -2,31 +2,34 @@
 
 #include "system/systhread.h"
 
+//#define SYS_THREAD_TRACE(msg...) ht_printf("[sys/thread] " msg)
+#define SYS_THREAD_TRACE(msg...) 
+
 int sys_create_mutex(sys_mutex *m) {
-    printf("sys_create_mutex()\n");
+    SYS_THREAD_TRACE("sys_create_mutex()\n");
     return 0;
 }
 
 int sys_create_semaphore(sys_semaphore *s) {
-    printf("sys_create_semaphore()\n");
+    SYS_THREAD_TRACE("sys_create_semaphore()\n");
     return 0;
 }
 
 int sys_create_thread(sys_thread *t, int flags, sys_thread_function start_routine, void *arg) {
-    printf("sys_create_thread()\n");
+    SYS_THREAD_TRACE("sys_create_thread()\n");
     return 0;
 }
 
 void sys_destroy_mutex(sys_mutex m) {
-    printf("sys_destroy_mutex()\n");
+    SYS_THREAD_TRACE("sys_destroy_mutex()\n");
 }
 
 void sys_destroy_semaphore(sys_semaphore s) {
-    printf("sys_destroy_semaphore()\n");
+    SYS_THREAD_TRACE("sys_destroy_semaphore()\n");
 }
 
 void sys_destroy_thread(sys_semaphore s) {
-    printf("sys_destroy_thread()\n");
+    SYS_THREAD_TRACE("sys_destroy_thread()\n");
 }
 
 int sys_lock_mutex(sys_mutex m) {
@@ -41,34 +44,34 @@ void sys_unlock_mutex(sys_mutex m) {
 }
 
 void sys_signal_semaphore(sys_semaphore s) {
-    printf("sys_signal_semaphore()\n");
+    SYS_THREAD_TRACE("sys_signal_semaphore()\n");
 }
 
 void sys_signal_all_semaphore(sys_semaphore s) {
-    printf("sys_signal_all_semaphore()\n");
+    SYS_THREAD_TRACE("sys_signal_all_semaphore()\n");
 }
 
 void sys_wait_semaphore(sys_semaphore s) {
-    printf("sys_wait_semaphore()\n");
+    SYS_THREAD_TRACE("sys_wait_semaphore()\n");
 }
 
 void sys_wait_semaphore_bounded(sys_semaphore s, int ms) {
-    printf("sys_wait_semaphore_bounded()\n");
+    SYS_THREAD_TRACE("sys_wait_semaphore_bounded()\n");
 }
 
 void sys_lock_semaphore(sys_semaphore s) {
-    printf("sys_lock_semaphore()\n");
+    SYS_THREAD_TRACE("sys_lock_semaphore()\n");
 }
 
 void sys_unlock_semaphore(sys_semaphore s) {
-    printf("sys_unlock_semaphore()\n");
+    SYS_THREAD_TRACE("sys_unlock_semaphore()\n");
 }
 
 void sys_exit_thread(void *ret) {
-    printf("sys_exit_thread()\n");
+    SYS_THREAD_TRACE("sys_exit_thread()\n");
 }
 
 void *sys_join_thread(sys_thread t) {
-    printf("sys_join_thread()\n");
+    SYS_THREAD_TRACE("sys_join_thread()\n");
     return nullptr;
 }
