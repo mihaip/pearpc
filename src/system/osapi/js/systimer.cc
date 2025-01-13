489e8e7c52c8bc0cbb4558c977b4ad41dfa11067
Mihai Parparita <mihai@persistent.info>
2025-01-12 20:19:57 -0800

    More directly use performance.now for sys_get_hiresclk_ticks()
    gettimeofday() does not have microsecond precision as implemented in Emscripten.

--- src/system/osapi/js/systimer.cc
+++ src/system/osapi/js/systimer.cc
@@ -1,12 +1,12 @@
-#include <sys/time.h>
+#include <emscripten.h>
 
 #include "system/sysclk.h"
 
 uint64 sys_get_hiresclk_ticks() {
-    struct timeval tv;
-    struct timezone tz;
-    gettimeofday(&tv, &tz);
-    return (uint64(tv.tv_sec) * 1000000) + tv.tv_usec;
+    double performanceNow = EM_ASM_DOUBLE({
+        return performance.now();
+    });
+    return uint64(performanceNow * 1000.0);
 }
 
 uint64 sys_get_hiresclk_ticks_per_second() {
